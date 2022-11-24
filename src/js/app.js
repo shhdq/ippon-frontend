61626205bf07216cada59192de46e737026a58d0
shhdq <54862478+shhdq@users.noreply.github.com>
2022-11-24 17:14:09 +0200

    test

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,11 @@ let nav = document.getElementById("mob-nav");
 let close = document.getElementById("close");
 
 ham.addEventListener("click", () => {
+  ham.style.display = "none";
   nav.classList.remove("hidden");
 });
+
+close.addEventListener("click", () => {
+  nav.classList.add('hidden');
+  ham.style.display = "flex";	
+})
